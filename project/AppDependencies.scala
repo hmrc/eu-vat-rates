575b952d7b6d2184de9ac8916c15ff1f621b57de
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2024-10-16 14:55:53 +0100

    VEIOSS-697 | Remove access logs and update scalacheck version.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,7 +17,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
     "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0",
-    "org.scalacheck" %% "scalacheck" % "1.17.0",
+    "org.scalacheck" %% "scalacheck" % "1.18.0",
   )
 
   val it = Seq.empty
