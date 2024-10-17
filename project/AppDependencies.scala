0f57871705435fd4d2c4d10891ae1dd7b73822f2
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2024-10-17 15:21:21 +0100

    VEIOSS-697 | Ensure scalacheck dependencies for test only.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,8 +16,8 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
-    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0",
-    "org.scalacheck" %% "scalacheck" % "1.18.0",
+    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % Test,
+    "org.scalacheck" %% "scalacheck" % "1.18.0" % Test
   )
 
   val it = Seq.empty
